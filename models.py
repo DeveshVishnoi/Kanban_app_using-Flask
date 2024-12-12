d11964ac4688cd72cf795680fef042aa1fc4790d
DeveshVishnoi <103602139+DeveshVishnoi@users.noreply.github.com>
2024-12-12 15:05:22 +0530

    Update models.py

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ import os
 
 app = Flask(__name__)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'kanaban.sqlite3')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
